af06639fb25132292af75703bd20af7091d150e9
Klaus Brandner <k.brandner@fivedesigns.dev>
2022-03-31 11:40:37 +0200

    Adding ngx-notifications library

---
+++ projects/ngx-notifications/src/lib/ngx-notifications.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-notifications',
+  template: `
+    <p>
+      ngx-notifications works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxNotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ngx-notifications/src/lib/ngx-notifications.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxNotificationsComponent } from './ngx-notifications.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxNotificationsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxNotificationsComponent
+  ]
+})
+export class NgxNotificationsModule { }
