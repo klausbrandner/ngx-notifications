219ae2e38f7fd196ea1a253f67f2285f7c33f038
Klaus Brandner <k.brandner@fivedesigns.dev>
2022-04-01 10:19:33 +0200

    Prepared demo to use ngx-notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxNotificationService } from 'ngx-notifications';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ngx-notifications';
+export class AppComponent implements OnInit {
+
+  constructor(private notificationService: NgxNotificationService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  showInfo(): void {
+    this.notificationService.info("Information", "This is just an information.");
+  }
+
+  showSuccess(): void {
+    this.notificationService.success("Success", "Changes successfully saved.");
+  }
+
+  showWarning(): void {
+    this.notificationService.warning("Warning", "Please save your changes first.");
+  }
+
+  showError(): void {
+    this.notificationService.error("Oh Snap!", "Could not save changes.");
+  }
 }
